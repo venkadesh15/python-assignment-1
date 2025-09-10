a451f4bd3db34ae5257ca59f58b5574162902c55
Venkadesh K <collegevenkadesh@gmail.com>
2025-09-10 20:50:06 +0530

    Delete mathematics operator.py

--- mathematics operator.py
+++
@@ -1,8 +0,0 @@
-a=input("Enter the first number=")
-b=input("Enter the second number=")
-a=int(a)
-b=int(b)
-print("addition =",a+b)
-print("subtraction =",a-b)
-print("multiplication =",a*b)
-print("division =",a/b)

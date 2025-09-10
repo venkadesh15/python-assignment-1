9e8b1d022f646fadcc5101319a1f532327e268d6
Venkadesh K <collegevenkadesh@gmail.com>
2025-09-10 20:52:14 +0530

    Add files via upload

---
+++ greeting.py
@@ -0,0 +1,3 @@
+a=input("Enter the first name :")
+b=input("Enter the second name :")
+print("Hello, "+ a +" "+ b + "! welcome to python program.")
---
+++ mathematics operator.py
@@ -0,0 +1,8 @@
+a=input("Enter the first number=")
+b=input("Enter the second number=")
+a=int(a)
+b=int(b)
+print("addition =",a+b)
+print("subtraction =",a-b)
+print("multiplication =",a*b)
+print("division =",a/b)
